98addc64c52a8451566790c3d9060a7033abb7e3
Student <redstoneman0000@gmail.com>
2022-10-27 20:18:28 +0500

    eche bolshe optimizated out programma

--- src/Speak.java
+++ src/Speak.java
@@ -1,10 +1,7 @@
 import java.util.List;
 import java.util.Random;
 public class Speak{
-    List<Animal> animallist;
     public Speak(List<Animal> animallist) {
-
-        this.animallist = animallist;
         while (true) {
             Random random = new Random();
             animallist.get(random.nextInt(animallist.size())).speak();
